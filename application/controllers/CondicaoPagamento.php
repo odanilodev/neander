0f6e1807fb4436825f7e94627887baccff61c952
guilhermebprd-dev <guilherme6788@hotmail.com>
2024-09-17 14:15:20 -0300

    CRUD PARA CADASTRO DE CONDIÇÕES DE PAGAMENTO UTILIZADAS NO MENU DE PREÇO DE VENDA

---
+++ application/controllers/CondicaoPagamento.php
@@ -0,0 +1,169 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class CondicaoPagamento extends CI_Controller
+{
+  public function __construct()
+  {
+    parent::__construct();
+
+    // Controle de sessão
+    $this->load->library('Controle_sessao');
+    $res = $this->controle_sessao->controle();
+    if ($res === 'erro') {
+      if ($this->input->is_ajax_request()) {
+        $this->output->set_status_header(403);
+        exit();
+      } else {
+        redirect('login/erro', 'refresh');
+      }
+    }
+
+    // Carregando o modelo CondicaoPagamento_model
+    $this->load->model('CondicaoPagamento_model');
+  }
+
+  /**
+   * Carrega a página inicial de Condição de Pagamento.
+   * Inclui scripts padrão e específicos da página.
+   */
+  public function index()
+  {
+    // Scripts padrão
+    $scriptsPadraoHead = scriptsPadraoHead();
+    $scriptsPadraoFooter = scriptsPadraoFooter();
+
+    // Scripts específicos para Condição de Pagamento
+    $scriptsCondicaoPagamentoFooter = scriptsCondicaoPagamentoFooter();
+
+    // Adicionando scripts ao header e footer
+    add_scripts('header', array_merge($scriptsPadraoHead));
+    add_scripts('footer', array_merge($scriptsPadraoFooter, $scriptsCondicaoPagamentoFooter));
+
+    // Obtendo todas as Condições de Pagamento
+    $data['condicoesPagamento'] = $this->CondicaoPagamento_model->recebeCondicoesPagamento();
+
+    // Carregando as views com os dados
+    $this->load->view('admin/includes/painel/cabecalho', $data);
+    $this->load->view('admin/paginas/condicao-pagamento/condicao-pagamento');
+    $this->load->view('admin/includes/painel/rodape');
+  }
+
+  /**
+   * Carrega o formulário de cadastro/edição de Condição de Pagamento.
+   * Inclui scripts padrão e específicos da página.
+   */
+  public function formulario()
+  {
+    // Scripts padrão
+    $scriptsPadraoHead = scriptsPadraoHead();
+    $scriptsPadraoFooter = scriptsPadraoFooter();
+
+    // Scripts específicos para Condição de Pagamento
+    $scriptsCondicaoPagamentoHead = scriptsCondicaoPagamentoHead();
+    $scriptsCondicaoPagamentoFooter = scriptsCondicaoPagamentoFooter();
+
+    // Adicionando scripts ao header e footer
+    add_scripts('header', array_merge($scriptsPadraoHead, $scriptsCondicaoPagamentoHead));
+    add_scripts('footer', array_merge($scriptsPadraoFooter, $scriptsCondicaoPagamentoFooter));
+
+    // Obtendo o ID da condição de pagamento da URL
+    $id = $this->uri->segment(3);
+
+    // Obtendo a condição de pagamento específica
+    $data['condicaoPagamento'] = $this->CondicaoPagamento_model->recebeCondicaoPagamento($id);
+
+    // Carregando as views com os dados
+    $this->load->view('admin/includes/painel/cabecalho', $data);
+    $this->load->view('admin/paginas/condicao-pagamento/cadastra-condicao-pagamento');
+    $this->load->view('admin/includes/painel/rodape');
+  }
+
+  /**
+   * Processa o cadastro ou edição de uma Condição de Pagamento.
+   * Verifica se o nome da Condição de Pagamento já existe e insere ou edita conforme necessário.
+   * Retorna uma resposta JSON para o JavaScript do lado do cliente.
+   */
+  public function cadastraCondicaoPagamento()
+  {
+    $id = (int) $this->input->post('id');
+
+    // Obtendo os dados do formulário e atribuindo ao array $dados
+    $dados = array(
+      'nome' => $this->input->post('nomeCondicaoPagamento'),
+      'id_empresa' => (int) $this->session->userdata('id_empresa')
+    );
+
+    // Verificando se a condição de pagamento já existe
+    $condicaoPagamento = $this->CondicaoPagamento_model->recebeNomeCondicaoPagamento($dados['nome'], $id);
+
+    if ($condicaoPagamento) {
+      // Resposta caso a condição de pagamento já exista
+      $response = array(
+        'title' => "Algo deu errado!",
+        'type' => "error",
+        'success' => false,
+        'message' => "Esta Condição de Pagamento já existe! Tente cadastrar uma diferente."
+      );
+
+      return $this->output->set_content_type('application/json')->set_output(json_encode($response));
+    }
+
+    // Inserindo ou editando a condição de pagamento
+    $retorno = $id ? $this->CondicaoPagamento_model->editaCondicaoPagamento($id, $dados) : $this->CondicaoPagamento_model->insereCondicaoPagamento($dados);
+
+    if ($retorno) {
+      // Resposta de sucesso
+      $response = array(
+        'success' => true,
+        'title' => 'Sucesso!',
+        'message' => $id ? 'Condição de Pagamento editada com sucesso!' : 'Condição de Pagamento cadastrada com sucesso!',
+        'type' => 'success'
+      );
+    } else {
+      // Resposta de erro
+      $response = array(
+        'success' => false,
+        'title' => 'Algo deu errado!',
+        'message' => $id ? "Erro ao editar a Condição de Pagamento!" : "Erro ao cadastrar a Condição de Pagamento!",
+        'type' => 'error'
+      );
+    }
+
+    return $this->output->set_content_type('application/json')->set_output(json_encode($response));
+  }
+
+  /**
+   * Processa a exclusão de uma Condição de Pagamento.
+   * Retorna uma resposta JSON para o JavaScript do lado do cliente.
+   */
+  public function deletaCondicaoPagamento()
+  {
+    // Obtendo o ID da condição de pagamento do formulário
+    $id = (int) $this->input->post('id');
+
+    // Deletando a condição de pagamento
+    $retorno = $this->CondicaoPagamento_model->deletaCondicaoPagamento($id);
+
+    if ($retorno) {
+      // Resposta de sucesso
+      $response = array(
+        'success' => true,
+        'title' => "Sucesso!",
+        'message' => "Condição de Pagamento deletada com sucesso!",
+        'type' => "success"
+      );
+    } else {
+      // Resposta de erro
+      $response = array(
+        'success' => false,
+        'title' => "Algo deu errado!",
+        'message' => "Não foi possível deletar a Condição de Pagamento!",
+        'type' => "error"
+      );
+    }
+
+    return $this->output->set_content_type('application/json')->set_output(json_encode($response));
+  }
+
+}
---
+++ application/views/admin/paginas/condicao-pagamento/cadastra-condicao-pagamento.php
@@ -0,0 +1,33 @@
+<div class="content">
+    <div class="row mb-9">
+        <div class="col-12">
+            <div class="card shadow-none border border-300 my-4">
+                <div class="card-header p-4 border-bottom border-300 bg-soft">
+                    <h4 class="text-900 mb-0"><?= $this->uri->segment(3) ? 'Editar Condição de Pagamento' : 'Cadastrar Condição de Pagamento'; ?></h4>
+                </div>
+
+                <div class="card-body p-4">
+                    <div class="row g-3">
+                        <input type="hidden" class="input-id" value="<?= $condicaoPagamento['id'] ?? ''; ?>">
+
+                        <!-- Nome -->
+                        <div class="col-md-12 mb-3">
+                            <label class="form-label">Nome</label>
+                            <input class="form-control input-nome input-obrigatorio input-nome-condicao-pagamento" type="text" placeholder="Nome da Condição de Pagamento" value="<?= $condicaoPagamento['nome'] ?? ''; ?>">
+                            <div class="d-none aviso-obrigatorio">Preencha este campo</div>
+                        </div>
+
+                        <!-- Botão Enviar -->
+                        <div class="col-12 text-end my-4">
+                            <button class="btn btn-phoenix-primary px-6 btn-envia" onclick="cadastraCondicaoPagamento()">
+                                <?= $this->uri->segment(3) ? 'Editar' : 'Cadastrar'; ?>
+                                <span class="fas fa-chevron-right" data-fa-transform="shrink-3"></span>
+                            </button>
+                            <div class="spinner-border text-primary load-form d-none" role="status"></div>
+                        </div>
+                    </div> <!-- .row.g-3 -->
+                </div> <!-- .card-body.p-4 -->
+            </div> <!-- .card -->
+        </div> <!-- .col-12 -->
+    </div> <!-- .row.mb-9 -->
+</div> <!-- .content -->
